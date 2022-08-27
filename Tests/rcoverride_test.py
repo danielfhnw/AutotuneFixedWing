9713e69e2c6f5dca87af66c599d3c66affe90e0b
danielfhnw <93777297+danielfhnw@users.noreply.github.com>
2022-08-27 16:45:51 +0200

    Create rcoverride_test.py

---
+++ Tests/rcoverride_test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 16 14:36:07 2022
+
+@author: ma1012017
+"""
+
+from pymavlink import mavutil
+import time
+
+master = mavutil.mavlink_connection("udp:127.0.0.1:3000", baud=500000)
+    
+master.wait_heartbeat()
+
+t = time.time()
+print("starting override")
+master.mav.command_long_send(master.target_system, master.target_component,
+                                   mavutil.mavlink.RC_CHANNELS_OVERRIDE, 
+                                   1100, 0, 0, 0, 0, 0, 0, 0)
+while time.time() - t < 10:
+    pass
+
+master.mav.command_long_send(master.target_system, master.target_component,
+                                   mavutil.mavlink.RC_CHANNELS_OVERRIDE, 
+                                   0, 0, 0, 0, 0, 0, 0, 0)
+print("stopped override")    
+master.close()
