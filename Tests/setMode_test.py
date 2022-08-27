5acd91d91806137dd2d239c6b294c9e4f0c4a5c1
danielfhnw <93777297+danielfhnw@users.noreply.github.com>
2022-08-27 15:16:53 +0200

    Update setMode_test.py

--- Tests/setMode_test.py
+++ Tests/setMode_test.py
@@ -21,7 +21,7 @@ while time.time() - t < 10:
     msg = master.recv_match(type="HEARTBEAT")
     if msg is not None:
         print(msg)
-        master.set_mode(81, 458752)
+    master.set_mode(81, 458752)
 
 master.set_mode_manual()
 print("manual mode")
