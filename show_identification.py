33c05984793c985aa12fee42f45a26f7b339d59b
danielfhnw <93777297+danielfhnw@users.noreply.github.com>
2022-09-03 23:55:54 +0200

    Update show_identification.py

--- show_identification.py
+++ show_identification.py
@@ -7,6 +7,7 @@ Created on Wed Aug 17 22:21:17 2022
 
 import pickle
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 def pickleLoader(pklFile):
@@ -23,22 +24,23 @@ servo = []
 time2 = []
 types = []
 
-with open("Logs/identlog5.pickle", "rb") as fp:
+with open("Logs/160811.pickle", "rb") as fp:
     for msg in pickleLoader(fp):
         if msg.get_type() == "ATTITUDE":
-            roll.append(msg.roll)
+            roll.append(msg.rollspeed)
             time.append(msg.time_boot_ms)
         elif msg.get_type() == "SERVO_OUTPUT_RAW":
             servo.append(msg.servo1_raw)
             time2.append(msg.time_usec)
+        elif msg.get_type() == "HEARTBEAT":
+            print(msg)
         types.append(msg.get_type())
             
-plt.figure(1)
-plt.plot(time,roll,'.')
-plt.grid(True)
-plt.show()
+for i,(t,s) in enumerate(zip(time2,servo)):
+    time2[i] = t/1000
+    servo[i] = (s-1000)/1000
 
-plt.figure(2)
+plt.plot(time,roll,'.')
 plt.plot(time2,servo,'.')
 plt.grid(True)
 plt.show()
@@ -49,4 +51,6 @@ diff = time[-1] - time[0]
 print(len(roll)/(diff/1000))
 
 diff = time2[-1] - time2[0]
-print(len(servo)/(diff/1000000))
+print(len(servo)/(diff/1000))
+
+#print(types)
